cad71196803f7c5aefbb1ef98b93b563fdf7e0b8
Dotsworthy <dotsworthy@gmail.com>
2020-04-28 20:43:33 +0100

    rendered devices

--- src/components/deviceDetail.js
+++ src/components/deviceDetail.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
 
 class DeviceDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPopup: false
+        }
+    }
 
     render() {
-        return null;
+        return (
+            <li>
+                <div>
+                    <h3>{this.props.deviceName}</h3>
+                    <p>{this.props.deviceType}</p> 
+                    <p>{this.props.id}</p> 
+                    <p>{this.props.ipAddress}</p> 
+                    <p>{this.props.macAddress}</p> 
+                    <p>{this.props.operatingSystem}</p> 
+                </div>
+            </li>
+        );
     }
 }
 
--- src/components/deviceList.js
+++ src/components/deviceList.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import DeviceDetail from './DeviceDetail.js'
 
 const DeviceList = (props) => {
     const deviceItems = props.devices.map(device => {
-        return <li> {device.hostName} </li>
-    })
         return (
-            <ul>
-            {deviceItems}
-            </ul>
+            <DeviceDetail
+            id={device.id} 
+            deviceName={device.hostName}
+            deviceType={device.deviceType}
+            ipAddress={device.ipAddress}
+            macAddress={device.macAddress}
+            operatingSystem={device.operatingSystem}
+            />
         )
+    })
+        
+    return (
+        <ul>
+        {deviceItems}
+        </ul>
+    )
      
 }
 
