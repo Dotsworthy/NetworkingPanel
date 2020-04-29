c87831dc382260314efa1f57621b30e61d81f219
Dotsworthy <dotsworthy@gmail.com>
2020-04-29 18:07:57 +0100

    added nearly complete chart

--- src/containers/NetworkContainer.js
+++ src/containers/NetworkContainer.js
@@ -6,6 +6,7 @@ class NetworkContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            chartData: [['Time', 'Upload Mbs', 'Download Mbs']],
             dark: false,
             devices: [
               {
@@ -55,10 +56,29 @@ class NetworkContainer extends Component {
         this.toggleMode = this.toggleMode.bind(this);
   }
 
-  collectDownloadSpeeds(searchTimestamp) {
-    const result = this.state.devices.filter(device => device.timeStamps.timeStamp === searchTimestamp)
-    console.log(result)
+  chartDataMapping(number) {
+    let newChartData = ['']
+    let uploadTotal = 0
+    let downloadTotal = 0
+    for (let counter = 0; counter < number; counter ++) {
+      this.state.devices.forEach(device => {
+        uploadTotal += device.timeStamps[counter].uploadSpeed
+        downloadTotal += device.timeStamps[counter].downloadSpeed
+      })
+    }
+      newChartData.push(uploadTotal)
+      newChartData.push(downloadTotal)
+      this.state.chartData.push(newChartData)
+      newChartData = ['']
   }
+ 
+  
+
+  //map through each device
+
+  //find the first timestamps object
+
+  //
 
     // countWiredDevices() {
     //   let wiredDevices = this.state.staticDevices.filter(device => device.connectionType === "wifi")
@@ -79,6 +99,7 @@ class NetworkContainer extends Component {
         return (
             <div className={this.state.dark ? 'network-dark' : 'network-light'}>
               <div className="content">
+                {this.chartDataMapping(2)}
                 <h1>Network Dashboard</h1>
                 <hr></hr>
                 <h2>Summary</h2>
