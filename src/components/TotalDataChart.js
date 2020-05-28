2ef303646732bbd6d8429e6aac66dc81c6774087
Dotsworthy <dotsworthy@gmail.com>
2020-05-28 16:46:17 +0100

    minor google chart functionality

--- src/components/TotalDataChart.js
+++ src/components/TotalDataChart.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import Chart from 'react-google-charts';
 
+const options = {
+        title: 'Total Upload & Downloads',
+        height: '100%',
+        width: '100%',
+        backgroundColor: { fill: 'transparent'},
+        colors: ['red', 'green'],
+        lineWidth: 3,
+        legend: { position: 'bottom'} ,
+        hAxis: { title: 'Time', titleTextStyle: { color: '#333'}, textStyle: {
+            fontSize: 9
+        } },
+        vAxis: { title: 'Mbs', minValue: 0 },
+        series: {
+            1: { curveType: 'function' },
+        },
+}
+
+
+
 const TotalDataChart = (props) => {
 
     return(
@@ -9,19 +28,7 @@ const TotalDataChart = (props) => {
             chartType="LineChart"
             loader={<div>Loading Chart</div>}
             data={ props.chartData }
-            options={{
-                title: 'Total Upload & Downloads',
-                height: '100%',
-                width: '100%',
-                backgroundColor: { fill: 'transparent'},
-                hAxis: { title: 'Time', titleTextStyle: { color: '#333'}, textStyle: {
-                    fontSize: 9
-                } },
-                vAxis: { title: 'Mbs', minValue: 0 },
-                series: {
-                    1: { curveType: 'function' },
-                },
-            }}
+            options={options}
         />
     </div>
     )
